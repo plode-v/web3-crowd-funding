8b11ebbbb8e38d6c660b23c68dcaf5ff4e7db4c7
Plode <84858705+plode-v@users.noreply.github.com>
2023-04-25 21:06:48 +0700

    update NavBar.jsx and create CustomButton.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
             <div className='sm:flex hidden mr-10 relative'>
                 <Sidebar />
             </div>
+            
             <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
                 <Navbar />
 
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -18,12 +18,12 @@ const Sidebar = () => {
     const navigate = useNavigate();
     const [isActive, setIsActive] = useState("dashboard");
     return (
-        <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+        <div className='flex justify-between items-center flex-col sticky top-5 h-[93h]'>
             <Link to="/">
                 <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
             </Link>
 
-            <div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[72px] py-4 mt-12'>
+            <div className='flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12'>
                 <div className="flex flex-col justify-center items-center gap-3">
                     {navlinks.map((link) => (
                         <Icon 
