b52108ae6939e03350355db0ec0e60e4af7bd430
Plode <84858705+plode-v@users.noreply.github.com>
2023-04-25 12:44:48 +0700

    Update Sidebar.jsx and add contexts for the webpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "./index.css"
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages"
+import { Route, Routes } from "react-router-dom";
+import { Sidebar, Navbar } from "./components"
 
 const App = () => {
     return (
-        <div><h1>Hello</h1></div>
+        <div className="relative sm:-8 p-4 bg-[#13131a] flex flex-row text-white">
+            <div className='sm:flex hidden mr-10 relative'>
+                <Sidebar />
+            </div>
+            <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+                <Navbar />
+
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/create' element={<CreateCampaign />} />
+                    <Route path='/campaign-details' element={<CampaignDetails />} />
+                </Routes>
+            </div>
+        </div>
     )
 }
 
